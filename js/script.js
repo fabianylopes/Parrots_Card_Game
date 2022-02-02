e9b138f8ed83e43215737e250fbce157f14d834e
Faby <fabiany.lopes.060@ufrn.edu.br>
2022-02-02 09:44:14 -0300

    cards randomness

--- js/script.js
+++ js/script.js
@@ -1,49 +1,39 @@
 let cardNumber;
 
 do{
-    cardNumber = parseInt(prompt('Com quantas cartas deseja jogar? '));
+    cardNumber = parseInt(prompt('Com quantas cartas deseja jogar? (números pares entre 4 e 14)'));
 }while(cardNumber % 2 !== 0 || cardNumber < 4 || cardNumber > 14);
 
 
 const parrots = ['bobross','explody','fiesta','metal','revertit','triplets','unicorn'];
 
-parrots.sort(compare);
 
 const cards = document.querySelector('.container');
 
-function startGame(){
+parrots.sort(compare);
 
-}
+let chosenCards = [];
 
 for (let i = 0; i < cardNumber / 2; i++) {
+    chosenCards.push(parrots[i]);
+    chosenCards.push(parrots[i]);
+}
 
+chosenCards.sort(compare);
+
+for (let i = 0; i < cardNumber; i++) {
     cards.innerHTML += `
         <div class="card" onclick="flip()">
             <div class="front-face face">
                 <img src="/assets/front.png">
             </div>
             <div class="back-face face">
-                <img src="/assets/${parrots[i]}parrot.gif">
+                <img src="/assets/${chosenCards[i]}parrot.gif">
             </div>
         </div>
-    `   
-
-    cards.innerHTML += `
-        <div class="card" onclick="flip()">
-            <div class="front-face face">
-                <img src="/assets/front.png">
-            </div>
-            <div class="back-face face">
-                <img src="/assets/${parrots[i]}parrot.gif">
-            </div>
-        </div>
-    `   
+    `     
 }
 
-
-function flip(){
-    
-}
 
 
 function compare() { 
