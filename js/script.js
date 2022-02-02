74752f04af6415d5d6b6fe48ab9e9415c3908550
Faby <fabiany.lopes.060@ufrn.edu.br>
2022-02-02 10:18:55 -0300

    click and turn

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-let cardNumber;
+let cardsNumber;
+
+let firstFlip = undefined;
+let secondFlip = undefined;
 
 do{
-    cardNumber = parseInt(prompt('Com quantas cartas deseja jogar? (números pares entre 4 e 14)'));
-}while(cardNumber % 2 !== 0 || cardNumber < 4 || cardNumber > 14);
-
+    cardsNumber = parseInt(prompt('Com quantas cartas deseja jogar? (números pares entre 4 e 14)'));
+}while(cardsNumber % 2 !== 0 || cardsNumber < 4 || cardsNumber > 14);
 
 const parrots = ['bobross','explody','fiesta','metal','revertit','triplets','unicorn'];
-
-
-const cards = document.querySelector('.container');
 
 parrots.sort(compare);
 
 let chosenCards = [];
 
-for (let i = 0; i < cardNumber / 2; i++) {
+for (let i = 0; i < cardsNumber / 2; i++) {
     chosenCards.push(parrots[i]);
     chosenCards.push(parrots[i]);
 }
 
 chosenCards.sort(compare);
 
-for (let i = 0; i < cardNumber; i++) {
+const cards = document.querySelector('.container');
+
+for (let i = 0; i < cardsNumber; i++) {
     cards.innerHTML += `
-        <div class="card" onclick="flip()">
+        <div class="card" onclick="flip(this)">
             <div class="front-face face">
                 <img src="/assets/front.png">
             </div>
@@ -34,6 +35,10 @@ for (let i = 0; i < cardNumber; i++) {
     `     
 }
 
+
+function flip(chosenCard){
+    chosenCard.classList.add('turned');
+}
 
 
 function compare() { 
